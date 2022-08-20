8198bee4eece7722567cef1d187c6e33ca7797e1
kolucy <lucychan20d@gmail.com>
2022-08-21 00:15:21 +0900

    WebSocket: Gateway Lifecycle

--- chattings/src/chats/chats.gateway.ts
+++ chattings/src/chats/chats.gateway.ts
@@ -1,13 +1,37 @@
+import { Logger } from '@nestjs/common';
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 
 @WebSocketGateway({ namespace: 'chattings' })
-export class ChatsGateway {
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  constructor() {
+    this.logger.log('constructor');
+  }
+
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    // 데코레이터로 인자를 받는다
+    this.logger.log(`disconnected : ${socket.id} ${socket.nsp.name}`); // nsp: namespace
+  }
+
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`connected : ${socket.id} ${socket.nsp.name}`); // nsp: namespace
+  }
+
+  afterInit() {
+    this.logger.log('init');
+  }
   @SubscribeMessage('new_user') // 데코레이터가 on으로 해당 이벤트의 메세지를 받음
   handleNewUser(
     @MessageBody() username: string,
